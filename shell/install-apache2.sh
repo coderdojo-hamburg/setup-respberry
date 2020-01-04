6b00a4eccdb9810f621d7322f879dec940affcb4
Marcel Domke <ma_domke@hotmail.com>
2020-01-04 13:05:00 +0100

    Update install-apache2.sh

--- shell/install-apache2.sh
+++ shell/install-apache2.sh
@@ -4,5 +4,8 @@ apt-get install apache2 -y
 a2enmod rewrite
 service apache2 restart
 
+# remove default index.html
+rm /var/www/html/index.html
+
 # Disable apache 2 on startup to prevent unintended 
 systemctl disable apache2
